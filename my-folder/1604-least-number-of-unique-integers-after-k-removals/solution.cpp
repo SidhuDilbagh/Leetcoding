aab7be43cb83f85cf2614aa91470ffe51cf78652
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2024-02-16 14:20:30 +0000

    [LeetCode Sync] Runtime - 83 ms (97.56%), Memory - 49.1 MB (99.06%)

---
+++ my-folder/1604-least-number-of-unique-integers-after-k-removals/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        sort(arr.begin(),arr.end());
+        vector<int> count;
+        count.push_back(1);
+        int i=0,n=arr.size();
+        for(int j=1;j<n;j++){
+            if(arr[j]==arr[j-1]){
+                count[i]++;
+            }
+            else{
+                count.push_back(1);
+                i++;
+            }
+        }
+        sort(count.begin(),count.end());
+        i=0;
+        n=count.size();
+        while(k && i<n){
+            k-=count[i];
+            if(k<0) return n-i;
+            i++;
+        }
+        return n-i;
+    }
+};
