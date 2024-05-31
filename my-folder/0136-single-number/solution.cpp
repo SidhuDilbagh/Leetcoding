ae0314ccb895852252b2a4d8add8eea046926807
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2024-05-31 09:48:42 +0000

    [LeetCode Sync] Runtime - 9 ms (88.08%), Memory - 19.3 MB (58.13%)

--- my-folder/0136-single-number/solution.cpp
+++ my-folder/0136-single-number/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int x:nums) ans^=x;
-        return ans;
+        int x=0;
+        for(int z:nums) x^=z;
+        return x;
     }
 };
