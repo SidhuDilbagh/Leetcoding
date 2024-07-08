7067367734e421b945f5c166b5d8dd47c5940596
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2024-07-08 08:21:25 +0000

    [LeetCode Sync] Runtime - 27 ms (61.59%), Memory - 38.1 MB (88.44%)

--- my-folder/0238-product-of-array-except-self/solution.cpp
+++ my-folder/0238-product-of-array-except-self/solution.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> answer={1};
-        int n=size(nums);
-        int num=nums[0];
-        for(int i=1;i<n;i++) {
-            answer.push_back(num);
-            num*=nums[i];
-        }
-        num=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            answer[i]*=num;
-            num*=nums[i];
+        int n=nums.size();
+        vector<int> answer(n,1);
+        int left=nums[0],right=nums[n-1];
+        for(int i=1,j=n-2;i<n;i++,j--){
+            answer[i]*=left;
+            left*=nums[i];
+            answer[j]*=right;
+            right*=nums[j];
         }
         return answer;
     }
