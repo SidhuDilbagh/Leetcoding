6f7f86060c7dde4cf2a2c61484ddca5bf5721144
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2024-07-12 07:09:04 +0000

    [LeetCode Sync] Runtime - 312 ms (14.13%), Memory - 167.4 MB (36.67%)

--- my-folder/2573-remove-nodes-from-linked-list/solution.cpp
+++ my-folder/2573-remove-nodes-from-linked-list/solution.cpp
@@ -1,19 +1,31 @@
 class Solution {
-public:
-    ListNode* removeNodes(ListNode* head) {
-        stack<int> mono;
-        ListNode* ptr=head;
-        while(ptr){
-            while(!mono.empty() && ptr->val>mono.top()) mono.pop();
-            mono.push(ptr->val);
-            ptr=ptr->next;
-        }
-        ListNode *newHead=NULL;
-        while(!mono.empty()){
-            ptr=new ListNode(mono.top(),newHead);
-            newHead=ptr;
-            mono.pop();
+    ListNode* reverse(ListNode* head){
+        ListNode *ptr=head,*newHead=NULL;
+        while(head){
+            ListNode *t=head->next;
+            head->next=newHead;
+            newHead=head;
+            head=t;
         }
         return newHead;
+    }
+public:
+    ListNode* removeNodes(ListNode* head) {
+        head=reverse(head);
+        ListNode *ptr=head;
+        int maximum=ptr->val;
+        while(ptr){
+            if(ptr->next && ptr->next->val<maximum){
+                ListNode *temp=ptr->next;
+                ptr->next=temp->next;
+                delete temp;
+            }
+            else{
+                ptr=ptr->next;
+            }
+            if(ptr) maximum=max(maximum,ptr->val);
+        }
+        // ListNode *p=head;while(p){cout<<p->val<<"     ";p=p->next;}
+        return reverse(head);        
     }
 };
