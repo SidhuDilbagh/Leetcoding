1b6a7a15359262f9bbb203042d0106b412d46121
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2024-07-31 17:32:08 +0000

    [LeetCode Sync] Runtime - 44 ms (59.51%), Memory - 51.1 MB (30.64%)

--- my-folder/0055-jump-game/solution.cpp
+++ my-folder/0055-jump-game/solution.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        bool possible[n];
+        // if(n==1) return true;
+        int maxPossible=nums[0];
         for(int i=0;i<n;i++){
-            possible[i]=false;
+            maxPossible = max(maxPossible,i+nums[i]);
+            if(maxPossible==i) break;
+            // cout<<maxPossible<<" ";
         }
-        possible[0]=true;
-        for(int i=0;i<n;i++){
-            if(possible[i])
-            for(int j=min(n-1,i+nums[i]);j>=i;j--){
-                if(j==n-1) return true;
-                possible[j]=true;
-            }
-
-            // for(bool b:possible)cout<<b;
-            // cout<<endl;
-        }
-        return false;
+        return n-1<=maxPossible;
     }
 };
