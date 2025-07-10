35403638413ccdb7aa57de77b41a2c8a95641def
SidhuDilbagh <108341635+SidhuDilbagh@users.noreply.github.com>
2025-07-10 15:01:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.2 MB (57.85%)

--- my-folder/0238-product-of-array-except-self/solution.cpp
+++ my-folder/0238-product-of-array-except-self/solution.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
         vector<int> answer(n,1);
         int left=nums[0],right=nums[n-1];
-        for(int i=1,j=n-2;i<n;i++,j--){
+        for(int i=1;i<n;i++){
             answer[i]*=left;
             left*=nums[i];
-            answer[j]*=right;
-            right*=nums[j];
+            answer[n-i-1]*=right;
+            right*=nums[n-i-1];
         }
         return answer;
     }
